31d286abb47f77c51e30f1ea11d41924a8ddd727
shaoxcs <shao.xue@outlook.com>
2021-01-31 22:36:17 -0800

    add basic index, login, profile page

---
+++ src/test/java/com/wbdvsp21shaox/serverjava/ServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbdvsp21shaox.serverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
